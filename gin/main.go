6092fc0c0e536a9aa72bb1aac26aad5c204f0659
tks98 <travis.kenneth.smith@gmail.com>
2023-08-13 21:44:09 -0500

    add tests

--- gin/main.go
+++ gin/main.go
@@ -2,52 +2,90 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"database/sql"
 	"errors"
 	"fmt"
-	"github.com/dslipak/pdf"
-	"github.com/gin-gonic/gin"
-	openai "github.com/sashabaranov/go-openai"
-	"golang.org/x/net/context"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/dslipak/pdf"
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+	openai "github.com/sashabaranov/go-openai"
+	"golang.org/x/net/context"
 )
 
-// Constant definitions
 const (
 	tempDirectory = "temp/"
 	templateDir   = "templates/*"
 )
 
 var (
-	openaiAPIKey = os.Getenv("OPENAI_API_KEY") // Retrieving the API Key from environment variables
+	openaiAPIKey = os.Getenv("OPENAI_API_KEY") // OpenAI API key
+	db           *sql.DB                       // Database instance
 )
 
 func main() {
 	r := setupRouter()
-	err := r.Run(":8080")
+	defer db.Close() // Close the database connection upon exit
+	r.Run(":8080")
+}
+
+// Initialize and set up SQLite database
+func init() {
+	var err error
+	db, err = sql.Open("sqlite3", "./summaries.db")
 	if err != nil {
-		return
+		log.Fatal(err)
+	}
+
+	// Create table if not already present
+	_, err = db.Exec(`
+        CREATE TABLE IF NOT EXISTS summaries (
+            pdf_hash TEXT PRIMARY KEY,
+            summary TEXT
+        )
+    `)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
-// setupRouter initializes the Gin router and its routes.
+// Retrieve summary from database using the PDF hash
+func getSummaryFromDB(pdfHash string) (string, error) {
+	var summary string
+	err := db.QueryRow("SELECT summary FROM summaries WHERE pdf_hash = ?", pdfHash).Scan(&summary)
+	return summary, err
+}
+
+// Save the summary to the database
+func saveSummaryToDB(pdfHash, summary string) error {
+	_, err := db.Exec("INSERT INTO summaries (pdf_hash, summary) VALUES (?, ?)", pdfHash, summary)
+	return err
+}
+
+// Generate hash for content (used for caching mechanism)
+func hashContent(content string) string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(content)))
+}
+
+// Set up Gin routes and middleware
 func setupRouter() *gin.Engine {
 	r := gin.Default()
-
 	r.LoadHTMLGlob(templateDir)
 	r.GET("/", showUploadPage)
 	r.POST("/upload", handleFileUpload)
-
 	return r
 }
 
-// showUploadPage handles the root GET request to render the upload page.
+// Show the file upload page
 func showUploadPage(c *gin.Context) {
 	c.HTML(http.StatusOK, "upload.html", nil)
 }
 
-// handleFileUpload processes the uploaded file, extracts and summarizes its content.
+// Handle file upload, extraction, and summarization
 func handleFileUpload(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
@@ -56,9 +94,9 @@ func handleFileUpload(c *gin.Context) {
 		return
 	}
 
-	// Save the file to a temporary location
+	// Save the file temporarily
 	tempPath := tempDirectory + file.Filename
-	defer os.Remove(tempPath) // ensure temp file gets removed even if there's an error
+	defer os.Remove(tempPath)
 
 	if err = c.SaveUploadedFile(file, tempPath); err != nil {
 		logError("saving file", err)
@@ -73,6 +111,15 @@ func handleFileUpload(c *gin.Context) {
 		return
 	}
 
+	pdfHash := hashContent(text)
+
+	// Check cache before generating a new summary
+	cachedSummary, err := getSummaryFromDB(pdfHash)
+	if err == nil && cachedSummary != "" {
+		c.HTML(http.StatusOK, "summary.html", gin.H{"summary": cachedSummary})
+		return
+	}
+
 	summary, err := openaiSummarization(text)
 	if err != nil {
 		logError("summarizing text", err)
@@ -80,10 +127,15 @@ func handleFileUpload(c *gin.Context) {
 		return
 	}
 
+	// Cache the generated summary
+	if err := saveSummaryToDB(pdfHash, summary); err != nil {
+		logError("saving summary to database", err)
+	}
+
 	c.HTML(http.StatusOK, "summary.html", gin.H{"summary": summary})
 }
 
-// extractTextFromPDF processes the provided file path to extract text content from a PDF.
+// Extract text content from PDF
 func extractTextFromPDF(filePath string) (string, error) {
 	f, err := pdf.Open(filePath)
 	if err != nil {
@@ -104,9 +156,18 @@ func extractTextFromPDF(filePath string) (string, error) {
 	return buf.String(), nil
 }
 
-// openaiSummarization uses OpenAI to generate a summary of the provided text.
+// Summarize the text content using OpenAI
 func openaiSummarization(text string) (string, error) {
+	const maxTokens = 4000
+	const truncationMessage = "Note: The original text was truncated due to context length limits."
+
 	client := openai.NewClient(openaiAPIKey)
+
+	// Truncate text if too long
+	if len(text) > maxTokens {
+		text = text[:maxTokens] + "..."
+	}
+
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
@@ -124,10 +185,13 @@ func openaiSummarization(text string) (string, error) {
 		return "", err
 	}
 
+	if len(text) > maxTokens {
+		return resp.Choices[0].Message.Content + " " + truncationMessage, nil
+	}
 	return resp.Choices[0].Message.Content, nil
 }
 
-// logError is a utility function to format and log errors.
+// Log errors with a specified action context
 func logError(action string, err error) {
 	log.Printf("Error %s: %v", action, err)
 }
--- gin/tldr_test.go
+++ gin/tldr_test.go
@@ -1,1 +1,132 @@
 package main
+
+import (
+	"bytes"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+// Setup the test environment
+func setupRouterForTesting() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	r := setupRouter()
+	return r
+}
+
+func createMultipartRequest(url, fieldName, fileName string) (*http.Request, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	body := &bytes.Buffer{}
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(fieldName, file.Name())
+	if err != nil {
+		return nil, err
+	}
+	_, err = io.Copy(part, file)
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("POST", url, body)
+	request.Header.Set("Content-Type", writer.FormDataContentType())
+	return request, err
+}
+
+// Test PDF Upload functionality
+func TestPDFUpload(t *testing.T) {
+	r := setupRouterForTesting()
+
+	// Test valid PDF upload
+	request, err := createMultipartRequest("/upload", "file", "test_data/sample.pdf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	// Test invalid file type
+	request, err = createMultipartRequest("/upload", "file", "test_data/sample.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	response = httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.NotEqual(t, http.StatusOK, response.Code)
+}
+
+// Test Summary Generation
+func TestSummaryGeneration(t *testing.T) {
+	r := setupRouterForTesting()
+
+	request, err := createMultipartRequest("/upload", "file", "test_data/sample.pdf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	body, _ := ioutil.ReadAll(response.Body)
+	assert.Contains(t, string(body), "summary") // Check if the summary is present in the response
+}
+
+// Test Exception Handling
+func TestExceptionHandling(t *testing.T) {
+	r := setupRouterForTesting()
+
+	// Empty file
+	emptyFile := bytes.NewBuffer(nil)
+	request, _ := http.NewRequest(http.MethodPost, "/upload", emptyFile)
+	request.Header.Add("Content-Type", "multipart/form-data")
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.NotEqual(t, http.StatusOK, response.Code)
+}
+
+// Test Database Cache
+func TestDatabaseCache(t *testing.T) {
+	r := setupRouterForTesting()
+
+	// First request
+	request, err := createMultipartRequest("/upload", "file", "test_data/sample.pdf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	response := httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+
+	// Second request
+	request, err = createMultipartRequest("/upload", "file", "test_data/sample.pdf")
+	if err != nil {
+		t.Fatal(err)
+	}
+	response = httptest.NewRecorder()
+
+	r.ServeHTTP(response, request)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestMain(m *testing.M) {
+	// Setup and teardown for tests can go here
+	os.Exit(m.Run())
+}
